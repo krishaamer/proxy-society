21a32312b06d4359a9e0742077d4014ac3d78a03
Kris Haamer <kris@mindworks.ee>
2026-08-12 12:05:34 +0300

    Add Chinese route to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,10 +3,28 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://proxysociety.org",
+      url: "https://proxysociety.org/",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
+      alternates: {
+        languages: {
+          en: "https://proxysociety.org/",
+          "zh-Hant-TW": "https://proxysociety.org/zh-tw",
+        },
+      },
+    },
+    {
+      url: "https://proxysociety.org/zh-tw",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+      alternates: {
+        languages: {
+          en: "https://proxysociety.org/",
+          "zh-Hant-TW": "https://proxysociety.org/zh-tw",
+        },
+      },
     },
   ];
 }
